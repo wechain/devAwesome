e03f16f59d9180cb1c1b9b19e3be804f24145c48
ldrovira <luisdanielrovira8@gmail.com>
2018-10-21 01:33:38 -0400

    deploy

---
+++ precache-manifest.c110b6e977cc1047748fa552dccaa323.js
@@ -0,0 +1,364 @@
+self.__precacheManifest = [
+  {
+    "revision": "79daae3a271f78ca4dbdbcc9407d80ca",
+    "url": "/devAwesome/icons/font/Flaticon.svg"
+  },
+  {
+    "revision": "0ab22861f52da826edb5570454418bda",
+    "url": "/devAwesome/vuesax-logo.png"
+  },
+  {
+    "revision": "f2434851c24eec4ffad5221040a00fe3",
+    "url": "/devAwesome/icons/font/Flaticon.ttf"
+  },
+  {
+    "revision": "e591551122828966a3b1",
+    "url": "/devAwesome/css/chunk-0067.5a779ac2.css"
+  },
+  {
+    "revision": "7f46bfab103dce42ab92",
+    "url": "/devAwesome/css/chunk-0c4e.79c5cdc7.css"
+  },
+  {
+    "revision": "2ee8743dbec85f9a031668344d89ab9d",
+    "url": "/devAwesome/vuesax-logo-vertical.png"
+  },
+  {
+    "revision": "b580b4f2e716990a677c",
+    "url": "/devAwesome/css/chunk-1253.13ac82c0.css"
+  },
+  {
+    "revision": "b01d795887750193bf4ca1a0585440fb",
+    "url": "/devAwesome/icons/license/license.pdf"
+  },
+  {
+    "revision": "3ccbb906409a0ca31a47",
+    "url": "/devAwesome/css/chunk-1a91.0d67844b.css"
+  },
+  {
+    "revision": "0b39d0f337f3555a47c072f20b2220ed",
+    "url": "/devAwesome/icons/font/flaticon.html"
+  },
+  {
+    "revision": "558bc6edb5a2220e9041",
+    "url": "/devAwesome/css/chunk-1af3.5329bf9a.css"
+  },
+  {
+    "revision": "b866a4246e99b490c0c0c9e471ebc8af",
+    "url": "/devAwesome/png/Asset 28@144x.png"
+  },
+  {
+    "revision": "9d6b43ec73cfc8565ea8",
+    "url": "/devAwesome/css/chunk-23c4.880a09f4.css"
+  },
+  {
+    "revision": "9b80573142961f0e7a6e4dd0b065c868",
+    "url": "/devAwesome/png/Asset 25@144x.png"
+  },
+  {
+    "revision": "50efba52179dd779f783",
+    "url": "/devAwesome/css/chunk-3871.005f0b72.css"
+  },
+  {
+    "revision": "53c3124a18660325438fdeae02fec2f2",
+    "url": "/devAwesome/png/Asset 18@144x.png"
+  },
+  {
+    "revision": "f2e70c77e2c8348c4bf9",
+    "url": "/devAwesome/css/chunk-41cd.2e471ab9.css"
+  },
+  {
+    "revision": "087f443447e9e9f36c0a188813b5b491",
+    "url": "/devAwesome/png/Asset 10@144x.png"
+  },
+  {
+    "revision": "dc3dc329b037228c8e6e",
+    "url": "/devAwesome/css/chunk-6102.de0487d4.css"
+  },
+  {
+    "revision": "d9e2d45e272ba9b7f06d339f04e4ba4b",
+    "url": "/devAwesome/svg/Asset 55@144x.svg"
+  },
+  {
+    "revision": "d0d0e796403850f15959",
+    "url": "/devAwesome/css/chunk-64c5.db37ca85.css"
+  },
+  {
+    "revision": "3971b4239b7df21e773f29db6f159e00",
+    "url": "/devAwesome/svg/Asset 54@144x.svg"
+  },
+  {
+    "revision": "9a7add8c481d3c5b9db2",
+    "url": "/devAwesome/css/chunk-7744.e6bcd96e.css"
+  },
+  {
+    "revision": "2e364ea9e6390292e1d4cfcda451b06a",
+    "url": "/devAwesome/svg/Asset 53@144x.svg"
+  },
+  {
+    "revision": "27c4e763aba7a9dea48e",
+    "url": "/devAwesome/css/chunk-7765.a982cdd7.css"
+  },
+  {
+    "revision": "5600ada83f9f71372961fc647322ef60",
+    "url": "/devAwesome/svg/Asset 52@144x.svg"
+  },
+  {
+    "revision": "9fcb85886191ed57074a",
+    "url": "/devAwesome/css/chunk-7bfe.961d3b36.css"
+  },
+  {
+    "revision": "1ddaceada81eb23ff07424d489bc7457",
+    "url": "/devAwesome/svg/Asset 51@144x.svg"
+  },
+  {
+    "revision": "e12a60fcebfc60549d38",
+    "url": "/devAwesome/css/chunk-c6f8.413020ba.css"
+  },
+  {
+    "revision": "91f14e4f42be1ddd30befeec314ccc69",
+    "url": "/devAwesome/svg/Asset 50@144x.svg"
+  },
+  {
+    "revision": "50d99fee4f31162b72ab",
+    "url": "/devAwesome/css/chunk-e29c.5e60e9c7.css"
+  },
+  {
+    "revision": "08d1c16b2a4528c748b1b973d415aba7",
+    "url": "/devAwesome/svg/Asset 49@144x.svg"
+  },
+  {
+    "revision": "52c75fbad34852a5b984",
+    "url": "/devAwesome/css/chunk-f979.fb144c24.css"
+  },
+  {
+    "revision": "3591ca5b4ba29713a4f377426c24bc9c",
+    "url": "/devAwesome/svg/Asset 48@144x.svg"
+  },
+  {
+    "revision": "7ff026f8c5199d9cac94",
+    "url": "/devAwesome/css/chunk-vendors.662e3d85.css"
+  },
+  {
+    "revision": "e5d216bdacc33709d997b6aa0a1175e7",
+    "url": "/devAwesome/svg/Asset 47@144x.svg"
+  },
+  {
+    "revision": "79daae3a271f78ca4dbdbcc9407d80ca",
+    "url": "/devAwesome/img/Flaticon.79daae3a.svg"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/devAwesome/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/devAwesome/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/devAwesome/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/devAwesome/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/devAwesome/fonts/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/devAwesome/fonts/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/devAwesome/fonts/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "d76461d0bef9501cd7a5f6252a8c409d",
+    "url": "/devAwesome/index.html"
+  },
+  {
+    "revision": "886b333db775912ac87906784602d845",
+    "url": "/devAwesome/icons/font/flaticon.css"
+  },
+  {
+    "revision": "8b6ba565e665087294b2dfd2218db167",
+    "url": "/devAwesome/icons/font/Flaticon.eot"
+  },
+  {
+    "revision": "47dcb564a088e641ec8fa878c8eab5f3",
+    "url": "/devAwesome/svg/Asset 46@144x.svg"
+  },
+  {
+    "revision": "a2559ccae261950c4cc85514a61bfff8",
+    "url": "/devAwesome/svg/Asset 45@144x.svg"
+  },
+  {
+    "revision": "988444846a4c1cab390aab2aaaff2582",
+    "url": "/devAwesome/icons/font/Flaticon.woff"
+  },
+  {
+    "revision": "bd28bb99fb2e7486ac0f93e86747735b",
+    "url": "/devAwesome/svg/Asset 44@144x.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/devAwesome/robots.txt"
+  },
+  {
+    "revision": "d9e27dde1dece1a878b2ba99e8321fa0",
+    "url": "/devAwesome/png/Asset 29@144x.png"
+  },
+  {
+    "revision": "1a372cf388c9022f8e670683132caf26",
+    "url": "/devAwesome/devawesome-favicon-logo.png"
+  },
+  {
+    "revision": "506e5e30405f511d175a6bcf2b692e3a",
+    "url": "/devAwesome/png/devAwesome.png"
+  },
+  {
+    "revision": "f8d14ceb11832784a6519286112c3b61",
+    "url": "/devAwesome/png/Asset 27@144x.png"
+  },
+  {
+    "revision": "b1ceac56357a2a7b8ac44f7d797326ab",
+    "url": "/devAwesome/filters/angular.png"
+  },
+  {
+    "revision": "be9b743c05c27700e411a5b3aeca2d05",
+    "url": "/devAwesome/filters/github.png"
+  },
+  {
+    "revision": "95b200dde2941407059cc6199d0b3ee2",
+    "url": "/devAwesome/filters/react.png"
+  },
+  {
+    "revision": "6c75c517480990621565cf4777724635",
+    "url": "/devAwesome/filters/vue.png"
+  },
+  {
+    "revision": "0b7559123b6e95cda2285d3a30a757f7",
+    "url": "/devAwesome/icons/backup.txt"
+  },
+  {
+    "revision": "c4d87f9db136ca2d2bcf77dd438cd2a5",
+    "url": "/devAwesome/icons/font/_flaticon.scss"
+  },
+  {
+    "revision": "730e34b6b3c84d8d33dd565494de4323",
+    "url": "/devAwesome/patreon.png"
+  },
+  {
+    "revision": "ed927e4957f4a819354b3bdb409a6201",
+    "url": "/devAwesome/png/Asset 11@144x.png"
+  },
+  {
+    "revision": "ab7219b1f41eb84656dc350eb2baa69e",
+    "url": "/devAwesome/png/Asset 17@144x.png"
+  },
+  {
+    "revision": "0526cca1d6776060c4d0236be482c368",
+    "url": "/devAwesome/png/Asset 19@144x.png"
+  },
+  {
+    "revision": "fcc89000cd6c1f138dd70be0f2cd60d9",
+    "url": "/devAwesome/png/Asset 23@144x.png"
+  },
+  {
+    "revision": "1ca610f2a4e174b0aa4c3d071600671c",
+    "url": "/devAwesome/png/Asset 26@144x.png"
+  },
+  {
+    "url": "/devAwesome/chunk-7744.c095f4a7cea6701d8257.js"
+  },
+  {
+    "revision": "02d772225e3372d575ce",
+    "url": "/devAwesome/css/app.a7a36805.css"
+  },
+  {
+    "url": "/devAwesome/chunk-64c5.c095f4a7cea6701d8257.js"
+  },
+  {
+    "revision": "808122c47a93aa25d3d388f99820341f",
+    "url": "/devAwesome/apps/devawesome-blanco.png"
+  },
+  {
+    "revision": "728fe425dc15279f7c6cdae1f3f7088f",
+    "url": "/devAwesome/apps/lusaxweb-blanco.png"
+  },
+  {
+    "url": "/devAwesome/chunk-0067.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-0c4e.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-vendors.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-f979.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-e29c.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-c6f8.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-7bfe.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-7765.c095f4a7cea6701d8257.js"
+  },
+  {
+    "revision": "90575f3c385acfef9092f78729c4437e",
+    "url": "/devAwesome/apps/vueshare.png"
+  },
+  {
+    "revision": "319a59f426abc4fbcb091bbeb03524a4",
+    "url": "/devAwesome/apps/vuenut.png"
+  },
+  {
+    "url": "/devAwesome/chunk-6102.c095f4a7cea6701d8257.js"
+  },
+  {
+    "revision": "05af76365a8444d1219895c09d7bfb73",
+    "url": "/devAwesome/apps/vuesax.png"
+  },
+  {
+    "url": "/devAwesome/chunk-1253.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-41cd.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-3871.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-23c4.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-1af3.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/chunk-1a91.c095f4a7cea6701d8257.js"
+  },
+  {
+    "url": "/devAwesome/app.c095f4a7cea6701d8257.js"
+  },
+  {
+    "revision": "4bbf92f0fb7341493c58547e842510c8",
+    "url": "/devAwesome/Manuel-Rovira-Contreras.jpg"
+  },
+  {
+    "revision": "34e733845d64010addd109e41c027499",
+    "url": "/devAwesome/Luis-Daniel-Rovira-Contreras.jpg"
+  },
+  {
+    "revision": "28174a34e77bb5e5310ced9f95cb480b",
+    "url": "/devAwesome/Discord-Logo-White.png"
+  }
+];
